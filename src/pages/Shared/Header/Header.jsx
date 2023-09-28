108b89197f96f3092ff9fd6a1d7857d2a890a157
Mainul Islam <mainulislam6168@gmail.com>
2023-09-28 11:53:59 +0600

    created AuthProvider api and fix home & login-logout rendering

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
 import Marquee from "react-fast-marquee";
 import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
+import { FaCircleUser } from 'react-icons/fa6';
 
 const Header = () => {
+    const { user } = useContext(AuthContext);
     return (
         <Container className='mt-4'>
             <div className="text-center">
@@ -13,7 +17,7 @@ const Header = () => {
                 <p>{moment().format("dddd, MMMM D, YYYY")}</p>
             </div>
             <div className='d-flex'>
-         <Button variant="danger">Latest</Button>
+                <Button variant="danger">Latest</Button>
                 <Marquee className='bg-secondary-subtle' pauseOnHover={true} speed={100}>
                     I can be a React component, multiple React components, or just some text...I can be a React component, multiple React components, or just some text....
                 </Marquee>
@@ -23,15 +27,24 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mx-auto">
-                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#features">
+                                <Link className='text-decoration-none' to='/'>Home</Link>
+                            </Nav.Link>
                             <Nav.Link href="#pricing">About</Nav.Link>
                             <Nav.Link href="#pricing">Career</Nav.Link>
 
                         </Nav>
                         <Nav>
-                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            {user && <Nav.Link href="#deets">
+                                < FaCircleUser style={{ fontSize: '2rem' }} />
+                            </Nav.Link>}
                             <Nav.Link eventKey={2} href="#memes">
+                                { user ? 
+                                <Button className='py-1 px-4' variant="dark">LogOut</Button> :
+                                <Link to='/login'>
                                 <Button className='py-1 px-4' variant="dark">Login</Button>
+                                </Link>
+                                }
                             </Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
