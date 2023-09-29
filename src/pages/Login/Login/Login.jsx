651fd60f84a856dc436af0c57e2432af2a233aaa
Mainul Islam <mainulislam6168@gmail.com>
2023-09-29 09:14:56 +0600

    login and registration form structure done

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h2>this is login</h2>
-        </div>
+        <Container  className='mx-auto w-50 m-5 shadow-lg  p-5'>
+            <Form className=''>
+                <h2 className='text-secondary pb-3 mx-auto'>Login Your Account</h2> <hr />
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" name='email' placeholder="Enter your email address" required />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" name='password' placeholder=" Enter your password" required />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+
+                <Form.Text className="text-success">
+                  
+                </Form.Text>
+                <Form.Text className="text-danger">
+                    
+                </Form.Text>
+                <Button variant="secondary w-100 mb-3 fw-bold" type="submit">
+                    Login
+                </Button> 
+                <br />
+                <Form.Text className="text-secondary fw-semibold ">
+                 Don't Have An Account? Please <Link to='/register'>Register</Link>
+                </Form.Text>
+            </Form>
+        </Container>
     );
 };
 
--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import { Button, Container, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            <h2>This is Register</h2>
-        </div>
+        <Container className='mx-auto w-50 m-5 shadow-lg p-5 '>
+        <Form>
+            <h2 className='text-secondary pb-3'>Register Your Account</h2> <hr />
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Your Name</Form.Label>
+                <Form.Control type="text" name='name' placeholder="Enter your name" required />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Photo URL</Form.Label>
+                <Form.Control type="url" name='url' placeholder="Enter your photo url" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" name='email' placeholder="Enter your email address" required />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" name='password' placeholder=" Enter your password" required />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" name='accept' label="Accepts Terms And Conditions" required />
+            </Form.Group>
+
+            <Form.Text className="text-success">
+              
+            </Form.Text>
+            <Form.Text className="text-danger">
+                
+            </Form.Text>
+            <Button variant="secondary w-100 mb-3 fw-bold" type="submit">
+                Login
+            </Button> 
+            <br />
+            <Form.Text className="text-secondary fw-semibold ">
+             Already Have An Account? Please <Link to='/login'>Login</Link>
+            </Form.Text>
+        </Form>
+    </Container>
     );
 };
 
